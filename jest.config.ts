30d15a78f816747fad5b8b7e6d0d33016d88f9c6
AutanaSoft <admin@autanasoft.com>
2025-12-08 11:24:55 -0400

    chore(test): refactor jest configuration to use swc and typescript

---
+++ jest.config.ts
@@ -0,0 +1,47 @@
+import type { Config } from 'jest';
+
+const baseProject: Config = {
+  transform: {
+    '^.+\\.(t|j)s$': ['@swc/jest', { configFile: '.swcrc' }],
+  },
+  testEnvironment: 'node',
+  cacheDirectory: '.tmp/jestCache',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.(t|j)s',
+    '!<rootDir>/src/config/**/*.(t|j)s',
+    '!<rootDir>/src/main.ts',
+    '!<rootDir>/src/modules/database/**/*.(t|j)s',
+    '!<rootDir>/src/**/index.(t|j)s',
+  ],
+  clearMocks: true,
+  modulePaths: ['./'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+const config: Config = {
+  projects: [
+    {
+      ...baseProject,
+      displayName: 'unit',
+      testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    },
+    {
+      ...baseProject,
+      displayName: 'e2e',
+      testMatch: ['<rootDir>/test/**/*.e2e-spec.ts'],
+    },
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+};
+
+export default config;
